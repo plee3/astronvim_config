2b4adceaccf7fb8b104c17d6be13342b63b21b25
Patrick Lee <plee3@ct-debian-dev.example.local>
2024-05-31 14:06:38 -0700

    Turn off auto formatting on save

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,10 +1,26 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
 --       as this provides autocomplete and documentation while editing
 
+---@type LazySpec
+return {
+  "AstroNvim/astrolsp",
+  ---@type AstroLSPOpts
+  opts = {
+    -- customize lsp formatting options
+    formatting = {
+      -- control auto formatting on save
+      format_on_save = {
+        enabled = false, -- enable or disable format on save globally
+      },
+    },
+  },
+}
+
+--[[-- Original code
 ---@type LazySpec
 return {
   "AstroNvim/astrolsp",
@@ -104,3 +120,5 @@ return {
     end,
   },
 }
+]]
+--
